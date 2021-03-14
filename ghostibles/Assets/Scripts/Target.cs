0cc8bae005c1f272aff23e5989f2c5d609cb0860
yreyes6 <yazminreyes21@gmail.com>
2021-03-14 17:08:47 -0500

    ghost added and music player started

---
+++ ghostibles/Assets/Scripts/Target.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+	// magic influenced
+	public int health = 30;
+	// music influenced
+	public int immunity = 30;
+
+    private void Start(){
+    }
+
+    // check when to destroy objects 
+    private void Update(){
+    	if (health == 0){
+    		Destroy(gameObject);
+    	}
+    	if (immunity == 0){
+    		Destroy(gameObject);
+    	}
+    }
+
+    //this method is called whenever a collision is detected
+    private void OnCollisionEnter(Collision collision) {
+    	// check attack 
+        if (collision.gameObject.tag == "MagicAttack"){
+        	if (health > 0){
+        		health = health - 10;
+        	}
+        }
+        if (collision.gameObject.tag == "MusicAttack"){
+        	if (immunity > 0){
+        		immunity = immunity - 10;
+        	}
+        	
+        }
+    }
+}
