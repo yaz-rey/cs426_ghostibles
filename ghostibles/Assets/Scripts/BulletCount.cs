a468734cd0c79b81f09e110aa1a0ea8e1357e6ec
CS-Primavera <46756149+CS-Primavera@users.noreply.github.com>
2021-04-02 16:20:34 -0500

    Adding magical gun player
    yazmin & sofi's combination

---
+++ ghostibles/Assets/Scripts/BulletCount.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BulletCount : MonoBehaviour
+{
+    public Text bullet;
+    public int maxBullet = 10;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet.text = "Bullets Left: " + maxBullet;
+        // print("start point" + total_score);
+    }
+
+    //Add bullet. Called from Target.cs
+    public void AddBullet(){
+        print("AT ADDBULLET" + maxBullet);
+        maxBullet += 1;
+        print("MAXB "+ maxBullet);
+        //Start();
+    }
+
+    public void MinusBullet(int bullets)
+    {
+              
+        bullet.text = "Bullets Left: " + bullets;
+    }
+}
