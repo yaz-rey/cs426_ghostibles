aab40c4023dd263c737690201cf039341dc67469
CS-Primavera <46756149+CS-Primavera@users.noreply.github.com>
2021-04-08 22:36:50 -0500

    Revert "Adjusted volume"
    This reverts commit 294f7889dd0602cde05319f693ab3f4c02943e12.

--- ghostibles/Assets/Scripts/Target1.cs
+++ ghostibles/Assets/Scripts/Target1.cs
@@ -119,7 +119,6 @@ public class Target1 : MonoBehaviour
       public void StopChasing()
       {
           chasing = false;
-          GetComponent<AudioSource>().Stop();
       }
     
       private void rotateGhost()
